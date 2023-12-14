76c206040226eb92255723875c928a5d6eeccadd
patrykmatwiejuk <patryk.matwiejuk@gmail.com>
2023-12-15 00:19:23 +0100

    fixes after forked repo commits and prs problems

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,14 +1,3 @@
-import { loginPage } from "../pages/LoginPage";
-import { registerPage } from "../pages/RegisterPage";
-
-// Cypress.Commands.add("loginSession", (userCredentials: LoginCredentials) => {
-//   const { username, password } = userCredentials;
-//   cy.visit("/signin");
-//   loginPage.usernameInputField.type(username);
-//   loginPage.passwordInputField.type(password, { log: false });
-//   loginPage.signInButton.click();
-// });
-
 Cypress.Commands.add("login", (username: string, password: string) => {
   cy.get("#username").type(username);
   cy.get("#password").type(password, { log: false });
--- cypress/support/users.ts
+++ cypress/support/users.ts
@@ -1,34 +1,5 @@
 const defaultPassword = Cypress.env().defaultPassword;
 
-// interface UsersTemplate {
-//   username: string;
-//   password: string;
-// }
-
-// class User implements UsersTemplate {
-//   username: string;
-//   password = defaultPassword;
-
-//   constructor(username: string, password: string = defaultPassword) {
-//     this.username = username;
-//     this.password = password;
-//   }
-// }
-
-// const userKB = new User("Katharina_Bernier");
-// const userTB = new User("Tavares_Barrows");
-// const userA = new User("Allie2");
-// const userG = new User("Giovanna74");
-// const userJK = new User("Jessyca.Kuhic");
-
-// const Users = {
-//   userKB,
-//   userTB,
-//   userA,
-//   userG,
-//   userJK,
-// };
-
 const defaultUsernames = {
   userKB: "Katharina_Bernier",
   userTB: "Tavares_Barrows",
